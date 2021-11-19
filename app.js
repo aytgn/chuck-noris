4e156b63b08b1f9f30a9381977e7742180e9c1dc
Aytugan Özgan <aytgnozgn@gmail.com>
2021-11-19 10:27:35 +0300

    jokes

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
-document.querySelector('.get-jokes').addEventListener('click', getJokes);
+//grab button
+const button = document.querySelector(".get-jokes");
 
-function getJokes(e) {
-  const number = document.querySelector('input[type="number"]').value;
-
+//event listeners
+button.addEventListener("click", getJokes);
+//event handlers
+function getJokes(event) {
+  //get input number
+  const number = document.querySelector("input[type='number']").value;
+  //create xhr instance
   const xhr = new XMLHttpRequest();
-
-  xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
-
-  xhr.onload = function() {
-    if(this.status === 200) {
+  //get request,to chuck-noris-jokes html,async
+  xhr.open("GET", `http://api.icndb.com/jokes/random/${number}`, true);
+  //send that request
+  xhr.send();
+  //on response load ..
+  xhr.onload = function () {
+    //if response is okay
+    if (this.status === 200) {
+      //get responseText as response JSON
       const response = JSON.parse(this.responseText);
-      
-      let output = '';
-
-      if(response.type === 'success') {
-        response.value.forEach(function(joke){
-          output += `<li>${joke.joke}</li>`;
+      //empty output initialization
+      let output = ``;
+      //if response type prop is success
+      if (response.type == "success") {
+        //iterate through each value prop of response, and add every output as joke
+        response.value.forEach(function (item) {
+          console.log(item);
+          output += `<li>${item.joke}</li>`;
         });
       } else {
-        output += '<li>Something went wrong</li>';
+        //if not success show error
+        output += ` <li>Something went wrong</li> `;
       }
-
-      document.querySelector('.jokes').innerHTML = output;
+      //after output created, show it to user
+      document.querySelector(".jokes").innerHTML = output;
     }
-  }
-
-  xhr.send();
-
-  e.preventDefault();
+  };
+  event.preventDefault();
 }
